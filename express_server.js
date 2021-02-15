3c62df329463ba1cf5dfecab685dc1173455639f
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-15 19:52:54 +0000

    added /urls

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
