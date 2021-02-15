9cc21db9738e474a00427c5e0f99b196ece3ea16
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-15 23:14:18 +0000

    parser and new url form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+const generateRandomString = function() {
+  
+};
 
 app.set('view engine', 'ejs')
 
@@ -16,6 +26,9 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars)
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortenedURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortenedURL, longURL: urlDatabase[req.params.shortenedURL] };
