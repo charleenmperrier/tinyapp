a7f4ad77c401df23a8c600c97a20597e5b2642d1
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-16 16:11:28 +0000

    nodemon added for ease

--- express_server.js
+++ express_server.js
@@ -39,12 +39,18 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+  
+});
 
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`)
+  // console.log(req.body);
 });
 
 
